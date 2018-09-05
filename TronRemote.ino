bbffb104a35204e649b43ac1dcfa9b3da76d7288
kvasilakes <my427v8@hotmail.com>
2018-09-04 23:14:20 -0500

    cleanup

--- TronRemote.ino
+++ TronRemote.ino
@@ -4,38 +4,7 @@
 #include "printf.h"
 #include "RF24.h"
 #include "RF24_config.h"
-
-/*
- Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
- */
-
-/**
- * Example LED Remote
- *
- * This is an example of how to use the RF24 class to control a remote
- * bank of LED's using buttons on a remote control.
- *
- * On the 'remote', connect any number of buttons or switches from
- * an arduino pin to ground.  Update 'button_pins' to reflect the
- * pins used.
- *
- * On the 'led' board, connect the same number of LED's from an
- * arduino pin to a resistor to ground.  Update 'led_pins' to reflect
- * the pins used.  Also connect a separate pin to ground and change
- * the 'role_pin'.  This tells the sketch it's running on the LED board.
- *
- * Every time the buttons change on the remote, the entire state of
- * buttons is send to the led board, which displays the state.
- */
-
 #include <SPI.h>
-#include "nRF24L01.h"
-#include "RF24.h"
-#include "printf.h"
 
 // Set up nRF24L01 radio on SPI bus plus pins 15 & 14 (CE & CS)
 RF24 radio(15, 14); 
@@ -108,36 +77,4 @@ void loop(void)
     //printf("\n");
     delay(1000);
 }
-
-
-  
-//    // Get the current state of buttons, and
-//    // Test if the current state is different from the last state we sent
-//    int i = num_button_pins;
-//    bool different = false;
-//    while(i--)
-//    {
-//      uint8_t state = ! digitalRead(button_pins[i]);
-//
-//      if ( state != button_states[i] )
-//      {
-//        different = true;
-//        button_states[i] = state;
-//      }
-//    }
-//
-//    // Send the state of the buttons to the LED board
-//    if ( different )
-//    {
-//      printf("Now sending...");
-//      bool ok = radio.write( button_states, num_button_pins );
-//      if (ok)
-//        printf("ok\n\r");
-//      else
-//        printf("failed\n\r");
-//    }
-
-    // Try again in a short while
-//    delay(200);
-//
 
